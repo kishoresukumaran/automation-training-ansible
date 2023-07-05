f0fc5c4c70de866566ef781da795365ad0ef78b9
arista <atd-help@arista.com>
2023-07-05 09:34:03 +0000

    Lab1

--- Day-1/Lab-1/example.py
+++ Day-1/Lab-1/example.py
@@ -1,42 +1,37 @@
 import requests
 from pprint import PrettyPrinter
 
-# disable warnings about self-signed certs
+#To disable warning about self signed certificates
 import urllib3
 urllib3.disable_warnings()
 
-# USE YOUR IPs
-DEVICE_IPS = ['192.168.0.10',
-              '192.168.0.11',
-              '192.168.0.12',
-              '192.168.0.13',
-              '192.168.0.14',
-              '192.168.0.15',
-              '192.168.0.16',
-              '192.168.0.17'
-              ]
+#Inventory
+DEVICE_IPS = ['192.168.0.11','192.168.0.12','192.168.0.13','192.168.0.14','192.168.0.15','192.168.0.16','192.168.0.17','192.168.0.18']
 
-# TODO: USE YOUR ATD CREDENTIALS
-USERNAME = ''
-PASSWORD = ''
+#Device Credentials
+USERNAME = 'arista'
+PASSWORD = 'arista0ob7'
 
 if __name__ == '__main__':
-    payload = {'jsonrpc': '2.0',
-               'method': 'runCmds',
-               'params': {
-                 'version': 1,
-                 # TODO: Modify the list of commands to run on the switches
-                 'cmds': ["show clock"]
-               },
-               'id': '1'
-              }
+    payload = {'jsonrpc':'2.0',
+    'method':'runCmds',
+    'params':{
+        'version':1,
+        #List of commands to run on the switch
+        'cmds':["show hostname","show version"]
+    },
+    'id':'1'
+    }
 
-    pp = PrettyPrinter()
+pp = PrettyPrinter()
 
-    # TODO: Add/modify the below code to iterate through all the IPs in the list 'DEVICE_IPS'
-    device = DEVICE_IPS[0]
+for device in DEVICE_IPS:
     r = requests.post('https://{}:443/command-api'.format(device), json=payload, auth=(USERNAME, PASSWORD), verify=False)
     response = r.json()
-    pp.pprint(response)
-    # TODO: Print the output in the desired fortmat
-    # print(f"Example of formating a string in python: {response['result'][0]}")
+    fqdn = response['result'][0]['fqdn']
+    serial = response['result'][1]['serialNumber']
+    mac = response['result'][1]['systemMacAddress']
+    model = response['result'][1]['modelName']
+    # pp.pprint(response)
+    #print(f"Example of formating a string in python: {response['result'][0]}")
+    print(f"{fqdn} is a {model} with serial number {serial} and system MAC address {mac}")
